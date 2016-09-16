3ff2407b2fba89d81fe3013427f5a476bd289c06
Shaurya Sharma <shaurya@intra.ellipticlabs.com>
2016-09-16 21:44:59 +0200

    wip

--- HashTable.h
+++ HashTable.h
@@ -12,13 +12,14 @@ namespace ss
 	struct DefaultHash
 	{
         template<typename K>
-		size_t operator()(K &&k) const
+		size_t operator()(const K &k) const noexcept
 		{
 			return std::hash<K>{}(k);
 		}
 	};
 
-	template<typename K, typename V, typename HashFunc = DefaultHash>
+
+	template<typename K, typename V, typename HashFunc = DefaultHash>	
 	class HashTable
 	{
     private:
@@ -28,6 +29,7 @@ namespace ss
         using mapped_type = V;
         using value_type = std::pair<K, V>;
 		using chain_t = std::list<value_type>;
+		using bucket_t = std::vector<chain_t>;
         using hasher = HashFunc;
         using reference = value_type&;
         using const_reference = const value_type&;
@@ -37,10 +39,9 @@ namespace ss
         using difference_type = ptrdiff_t;
         static constexpr size_type DEFAULT_MAX_SIZE = 1<<10;
 
-        HashTable(size_type max_size = DEFAULT_MAX_SIZE) :
-          _MAX_SIZE( max_size )
-		, _buckets(max_size, chain_t{})
-        , _bucket_count( max_size )
+        HashTable(size_type num_buckets = DEFAULT_MAX_SIZE) :
+		  _buckets(num_buckets, chain_t{})
+        , _bucket_count(0)
         , _size( 0 )
         {
 			constexpr bool hash_func_returns_size_type = std::is_same<std::result_of<HashFunc(const K&)>::type, size_t>::value;
@@ -77,16 +78,22 @@ namespace ss
             return _hash_func;
         }
 
-        template<typename U>
-		mapped_type& operator[](U &&k)
+       
+		mapped_type& operator[](const key_type &k)
         {
-            return ( at_private( std::forward<U>(k), NOT_FOUND_POLICY::INSERT_KEY_VALUE) );
+            return at_private( const_cast<key_type&>(k), NOT_FOUND_POLICY::INSERT_KEY_VALUE);
         }
+
+		mapped_type& operator[](K &&k)
+		{
+			return at_private(std::move(k), NOT_FOUND_POLICY::INSERT_KEY_VALUE);
+		}
+
 
 
         mapped_type& at ( const key_type& k )
         {
-            return const_cast<mapped_type&>( at_private( k, NOT_FOUND_POLICY::THROW_EXCEPTION) );
+            return at_private( k, NOT_FOUND_POLICY::THROW_EXCEPTION);
         }
 
         const mapped_type& at ( const key_type& k ) const
@@ -100,7 +107,7 @@ namespace ss
             return bucket_private( k );
         }
 
-        size_type bucket_count( const key_type &k ) const noexcept
+        size_type bucket_count( ) const noexcept
         {
             return _bucket_count;
         }
@@ -112,9 +119,57 @@ namespace ss
                 throw std::out_of_range( "bucket index out of range" );
             }
 
-            const auto *node = _buckets[n];
-            return node->size();
+			_buckets[n].size();
         }
+
+
+
+		class iterator
+		{
+		public:
+			using self_type = iterator;
+			using reference = value_type&;
+			using pointer = value_type*;
+			using iterator_category = std::forward_iterator_tag;
+			using difference_type = ptrdiff_t;
+			typedef typename HashTable<K, V, HashFunc> container_t;
+
+			iterator(){}
+
+			self_type operator++()
+			{
+				return *this;
+			}
+
+			reference operator*()
+			{
+				return *this;
+			}
+
+			pointer operator->()
+			{
+				return this;
+			}
+
+			bool operator==(const self_type &other)
+			{
+				return _ptr = other._ptr;
+			}
+
+			bool operator !=(const self_type &other)
+			{
+				return _ptr != other._ptr;
+			}
+
+			self_type operator=(const self_type &other)
+			{
+				_ptr = other._ptr;
+				return *this;
+			}
+
+		private:
+			pointer _ptr;
+		};
 
     private:
 
@@ -123,7 +178,7 @@ namespace ss
         {
             size_type bucket_idx = bucket_private( std::forward<U>(k) );
             chain_t &bucket = _buckets[bucket_idx];
-			
+
 			bool key_found = false;
 			chain_t::iterator it;
 			if (!bucket.empty())
@@ -144,16 +199,17 @@ namespace ss
                 }
                 else if ( not_found_policy == NOT_FOUND_POLICY::INSERT_KEY_VALUE )
                 {
+					if (bucket.empty())
+						++_bucket_count;
 					std::pair<K, V> p;
 					p.first = std::forward<U>(k);
-					bucket.push_back(std::move(p));
+					bucket.push_back(std::move(p));				
+					++_size;
 					return bucket.back().second;
                 }
             }
-			else
-			{
-				return it->second;
-			}
+
+			return it->second;
         }
 
         template<typename U>
@@ -165,7 +221,6 @@ namespace ss
 
         size_type _size;
         size_type _bucket_count;
-        const size_type _MAX_SIZE;
 		std::vector<chain_t> _buckets;
         HashFunc _hash_func;
 	};
